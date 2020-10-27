7b6e6d6bb740eb04f76db43037dc9bffb059e95a
2hoyeong <44524462+2hoyeong@users.noreply.github.com>
2020-10-27 16:16:07 +0900

    [WEB] chore : sequelize, nodemon 설치
    sequelize와 nodemon을 설치했습니다.

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,11 @@ import express, { json, urlencoded } from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
-
 import indexRouter from './routes/index';
 import usersRouter from './routes/users';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 
