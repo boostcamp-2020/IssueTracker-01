7e4e9f7a0a9ecce47f78a6eae1731e41dc8901e2
2hoyeong <leehodud302@naver.com>
2020-11-02 15:43:13 +0900

    [WEB] feat : 마일스톤 목록 조회 서비스 구현
    마일스톤 목록을 조회하는 서비스를 구현했습니다.

--- backend/services/milestoneService.js
+++ backend/services/milestoneService.js
@@ -1,5 +1,16 @@
-const getMilestoneList = (req, res) => {
-  // TODO : 마일스톤을 조회하는 서비스를 만든다.
+import Milestone from '@models/milestoneModel';
+
+const getMilestoneList = async (req, res, next) => {
+  try {
+    const milestones = await Milestone.findAll({ attributes: ['title', 'dueDate'] });
+    res.json(milestones.map((milestone) => milestone.dataValues));
+  } catch (err) {
+    console.log(err);
+    next({
+      status: 500,
+      message: err,
+    });
+  }
 };
 
 export default { getMilestoneList };
