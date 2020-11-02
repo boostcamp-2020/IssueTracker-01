91a5f75c4b0138ae190b257d348fd44711865cd3
2hoyeong <leehodud302@naver.com>
2020-11-03 00:13:11 +0900

    [WEB] refactor : dotenv 파일 일부 config에 추가
    GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, JWT_SECRET 를 추가했습니다.

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,10 +1,14 @@
 import dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config({ path: '../.env' });
 
 export default {
   DB_HOST: process.env.DB_HOST,
   DB_USER: process.env.DB_USER,
   DB_PASSWORD: process.env.DB_PASSWORD,
   DB_NAME: process.env.DB_NAME,
+
+  GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+  GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+  JWT_SECRET: process.env.JWT_SECRET,
 };
