27a5d2a429c456a81c933e0d785b0b5fbb99e8d7
SeokHoKim <seokho0330@gmail.com>
2020-11-03 10:18:31 +0900

    [IOS] fix: 셀 크기 문제
    - itemSize를 통해 좌우 여백없이 셀 width 맞춤

--- ios/IssueTracker/IssueTracker/Controllers/IssueViewController.swift
+++ ios/IssueTracker/IssueTracker/Controllers/IssueViewController.swift
@@ -9,12 +9,15 @@ import UIKit
 
 class IssueViewController: UIViewController {
     private lazy var dataSource = makeDataSource()
-    private var items: [Int] = [0, 1, 2]
+    private var items: [Int] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
     
     @IBOutlet weak var collectionView: UICollectionView?
     override func viewDidLoad() {
         super.viewDidLoad()
         configureCollectionView()
+        configureCell()
+        configureFlowLayout()
+        applySnapshot()
     }
 }
 
@@ -22,26 +25,22 @@ class IssueViewController: UIViewController {
 extension IssueViewController {
     private struct StoryBoard {
         static let cell = "IssueMainCell"
-        static let header = "IssueMainHeader"
     }
 }
 
 // MARK: - Configure CollectionView
 extension IssueViewController: UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         // TODO: - Action for click issue cell
     }
     
     private func configureCollectionView() {
-        configureCell()
-        configureFlowLayout()
         collectionView?.delegate = self
         collectionView?.dataSource = dataSource
-        applySnapshot()
     }
     
     private func configureCell() {
-        let name = "IssueCollectionViewCell"
+        let name = String(describing: IssueCell.self)
         let nibName = UINib(nibName: name, bundle: nil)
         collectionView?.register(nibName, forCellWithReuseIdentifier: StoryBoard.cell)
     }
@@ -49,6 +48,7 @@ extension IssueViewController: UICollectionViewDelegate {
     private func configureFlowLayout() {
         guard let layout = collectionView?.collectionViewLayout as? UICollectionViewFlowLayout else { return }
         layout.sectionHeadersPinToVisibleBounds = true
+        layout.itemSize.width = view.frame.width
     }
 }
 
