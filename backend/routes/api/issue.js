06909e104b1315edcae0350c27f6f1546ad7c53e
ohmink <kkop5056@naver.com>
2020-11-03 17:39:55 +0900

    [Web] refactor: 이슈의 레이블 api 리팩토링 반환할 때 필요 없는 데이터를 삭제하고 유저 목록을 불러오는 함수를 삭제하였다.

--- backend/routes/api/issue.js
+++ backend/routes/api/issue.js
@@ -3,11 +3,10 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.get('/assignee', issueService.getAssigneeCandidates);
-router.patch('/assignee/:id', issueService.changeAssignee);
+router.patch('/assignee/:id', issueService.updateAssignee);
 router.post('/issueLabel/:id', issueService.addLabel);
 router.delete('/issueLabel/:id', issueService.removeLabel);
-router.delete('/issueLabel/all', issueService.removeAllLabel);
+router.delete('/issueLabel/all/:id', issueService.removeAllLabel);
 router.patch('/:id', issueService.updateTitle);
 router.patch('/:id/milestone', issueService.updateMilestone);
 router.post('/', issueService.create);
