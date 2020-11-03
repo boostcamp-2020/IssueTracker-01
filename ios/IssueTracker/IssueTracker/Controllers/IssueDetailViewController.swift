346f9d2242520dbdb4bdb345ee37117412046021
SeokHoKim <seokho0330@gmail.com>
2020-11-03 15:33:49 +0900

    [IOS] feat: IssueDetailViewController 생성
    - Storyboard 내에 .swift 연결

---
+++ ios/IssueTracker/IssueTracker/Controllers/IssueDetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  IssueDetailViewController.swift
+//  IssueTracker
+//
+//  Created by 김석호 on 2020/11/03.
+//
+
+import UIKit
+
+class IssueDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
