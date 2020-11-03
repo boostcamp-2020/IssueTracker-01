c65738e8f73ff8394a25a021743da1255a2d2bca
ohmink <kkop5056@naver.com>
2020-11-03 17:05:57 +0900

    [Web] feat: 이슈의 담당자, 레이블 API 구현 담당자를 지정하고 바꾸는 api, 레이블을 지정하고 삭제하는 api를 구현하였다.

--- backend/services/issueService.js
+++ backend/services/issueService.js
@@ -1,1 +1,77 @@
 // TODO - 이슈 서비스 작성
+import Issue from '@models/issueModel';
+import User from '@models/userModel';
+import IssueLabel from '@models/issueLabelModel';
+
+const getAssigneeCandidates = async (req, res) => {
+    try {
+        const result = await User.findAll({attributes: ['userId', 'profile_url']});
+        
+        return res.status(200).json({ data: result, message: "success" });
+    } catch (error) {
+        return res.status(400).json({ message: error.message });
+    }
+}
+
+const changeAssignee = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const result = await Issue.update({
+            assignees: req.body.userId
+          },
+          {
+            where: { issueId: id },
+          }
+        );
+        
+        return res.status(200).json({ data: result, message: "success" });
+    } catch (error) {
+        return res.status(400).json({ message: error.message });
+    }
+}
+
+const addLabel = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const newData = {
+        issueId: id,
+        name: req.body.name
+      };
+      const result = await IssueLabel.create(newData);
+
+      return res.status(200).json({ data: result, message: "success" });
+    } catch (error) {
+      return res.status(400).json({ message: error.message });
+    }
+}
+
+const removeLabel = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const result = await IssueLabel.destroy({
+            where: { issueId: id, name: req.body.labelName }
+        });
+  
+        return res.status(200).json({ data: result, message: "success" });
+      } catch (error) {
+        return res.status(400).json({ message: error.message });
+    }
+}
+
+const removeAllLabel = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const result = await IssueLabel.destroy({
+            where: { issueId: id }
+        });
+  
+        return res.status(200).json({ data: result, message: "success" });
+      } catch (error) {
+        return res.status(400).json({ message: error.message });
+    }
+}
+
+export default { getAssigneeCandidates, changeAssignee, addLabel, removeLabel, removeAllLabel };
