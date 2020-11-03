df064fec16ef4f6073cc6e68a1cec5ff32a6f298
ohmink <kkop5056@naver.com>
2020-11-03 17:04:11 +0900

    [Web] feat: 이슈의 담당자, 레이블 API 구현 담당자를 지정하고 바꾸는 api, 레이블을 지정하고 삭제하는 api를 구현하였다.

--- backend/routes/api/issue.js
+++ backend/routes/api/issue.js
@@ -3,4 +3,10 @@ import { Router } from 'express';
 
 const router = Router();
 
+router.get('/assignee', issueService.getAssigneeCandidates);
+router.patch('/assignee/:id', issueService.changeAssignee);
+router.post('/issueLabel/:id', issueService.addLabel);
+router.delete('/issueLabel/:id', issueService.removeLabel);
+router.delete('/issueLabel/all', issueService.removeAllLabel);
+
 export default router;
