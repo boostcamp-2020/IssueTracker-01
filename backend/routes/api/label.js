30cd08d45788b4fb7f8a780d3a892331033c0301
ohmink <kkop5056@naver.com>
2020-11-03 10:52:11 +0900

    [Web] refactor: 레이블 API url 변경 API 명세에 정리한 url로 변경하였다.

--- backend/routes/api/label.js
+++ backend/routes/api/label.js
@@ -3,9 +3,9 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.get('/getLabels', labelService.getLabels);
-router.post('/createLabel', labelService.createLabel);
-router.patch('/updateLabel', labelService.updateLabel);
-router.delete('/deleteLabel', labelService.deleteLabel);
+router.get('/api/label', labelService.getLabels);
+router.post('/api/label', labelService.createLabel);
+router.patch('/api/label', labelService.updateLabel);
+router.delete('/api/label', labelService.deleteLabel);
 
 export default router;
