c1aded90c859e7b65a12796aa3ce78a0a2d6835c
ohmink <kkop5056@naver.com>
2020-11-04 13:59:32 +0900

    [Web] style: 코드 순서 변경 get, post, patch, delete 순으로 코드 순서를 변경하였다.

--- backend/routes/api/issue.js
+++ backend/routes/api/issue.js
@@ -3,15 +3,18 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.patch('/assignee/:issueId', issueService.updateAssignee);
-router.post('/issueLabel/:issueId', issueService.addLabel);
-router.delete('/issueLabel/:issueId', issueService.removeLabel);
-router.delete('/issueLabel/all/:issueId', issueService.removeAllLabel);
-router.patch('/:issueId', issueService.updateTitle);
-router.patch('/:issueId/milestone', issueService.updateMilestone);
-router.patch('/status/:status/:issueId', issueService.updateIssueStatus);
-router.post('/', issueService.create);
 router.get('/', issueService.getIssueLists);
 router.get('/detailIssue/:issueId', issueService.detailIssue);
+
+router.post('/', issueService.create);
+router.post('/issueLabel/:issueId', issueService.addLabel);
+
+router.patch('/:issueId', issueService.updateTitle);
+router.patch('/assignee/:issueId', issueService.updateAssignee);
+router.patch('/:issueId/milestone', issueService.updateMilestone);
+router.patch('/status/:status/:issueId', issueService.updateIssueStatus);
+
+router.delete('/issueLabel/:issueId', issueService.removeLabel);
+router.delete('/issueLabel/all/:issueId', issueService.removeAllLabel);
 
 export default router;
