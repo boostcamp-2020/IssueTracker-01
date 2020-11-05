8e675d43fd3fc861f236a8e564248ef3515a23c0
SeokHoKim <seokho0330@gmail.com>
2020-11-05 15:48:30 +0900

    [IOS] feat: IssueFilterHeader 추가
    - 필터 뷰 헤더 적용

--- ios/IssueTracker/IssueTracker/Controllers/Issue/IssueFilterViewController.swift
+++ ios/IssueTracker/IssueTracker/Controllers/Issue/IssueFilterViewController.swift
@@ -9,7 +9,9 @@ import UIKit
 
 class IssueFilterViewController: UIViewController {
     private lazy var dataSource = makeDataSource()
-    private var items = ["열린 이슈들", "내가 작성한 이슈들", "나한테 할당된 이슈들", "내가 댓글을 남긴 이슈들", "닫힌 이슈들"]
+    private var items = [["열린 이슈들", "내가 작성한 이슈들", "나한테 할당된 이슈들", "내가 댓글을 남긴 이슈들", "닫힌 이슈들"],
+                         ["작성자", "레이블", "마일스톤", "담당자"]]
+    private var sectionTitles = ["다음 중에 조건을 고르세요", "세부 조건"]
     @IBOutlet weak var collectionView: UICollectionView?
     
     override func viewDidLoad() {
@@ -29,7 +31,7 @@ class IssueFilterViewController: UIViewController {
 extension IssueFilterViewController {
     private struct ViewID {
         static let cell = String(describing: IssueFilterMainCell.self)
-        static let header = "IssueFilterHeader"
+        static let header = String(describing: IssueFilterHeader.self)
     }
 }
 
@@ -54,31 +56,35 @@ extension IssueFilterViewController: UICollectionViewDelegate {
 
 // MARK: - UICollectionViewDiffableDataSource
 extension IssueFilterViewController {
-    typealias DataSource = UICollectionViewDiffableDataSource<Int, String>
-    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, String>
+    typealias DataSource = UICollectionViewDiffableDataSource<String, String>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<String, String>
     
     private func makeDataSource() -> DataSource {
         guard let collectionView = collectionView else { return DataSource() }
-        let dataSource = DataSource(collectionView: collectionView, cellProvider: { (collectionView, indexPath, data) -> IssueFilterMainCell? in
+        let dataSource = DataSource(collectionView: collectionView, cellProvider: { (collectionView, indexPath, data) -> UICollectionViewCell? in
             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ViewID.cell, for: indexPath)
-            guard let listCell = cell as? IssueFilterMainCell else { return IssueFilterMainCell() }
-            listCell.configureView(title: data)
-            listCell.indentsAccessories = true
-            return listCell
+            guard let filterCell = cell as? IssueFilterMainCell else { return cell }
+            filterCell.configureView(title: data)
+            filterCell.indentsAccessories = true
+            return filterCell
         })
         
-        dataSource.supplementaryViewProvider = { collectionView, kind, indexPath in
+        dataSource.supplementaryViewProvider = { (collectionView, kind, indexPath) -> UICollectionReusableView? in
             guard kind == UICollectionView.elementKindSectionHeader else { return nil }
             let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: ViewID.header, for: indexPath)
-            return header
+            guard let filterHeader = header as? IssueFilterHeader else { return header }
+            filterHeader.configureView(title: self.sectionTitles[indexPath.section])
+            return filterHeader
         }
         return dataSource
     }
     
     private func applySnapshot(animatingDifferences: Bool = true) {
         var snapshot = Snapshot()
-        snapshot.appendSections([0])
-        snapshot.appendItems(items, toSection: 0)
+        snapshot.appendSections(sectionTitles)
+        (0...1).forEach {
+            snapshot.appendItems(items[$0], toSection: sectionTitles[$0])
+        }
         dataSource.apply(snapshot, animatingDifferences: animatingDifferences)
     }
     
