3b3c97ad77e38b0733d2a620d5e67a192de94df1
nrurnru <nrurnru@gmail.com>
2020-11-08 14:57:07 +0900

    [iOS] feat : 이슈 상세 화면 코멘트 셀 기본 화면 구현
    diffableDatasource 구현 필요

--- ios/IssueTracker/IssueTracker/Controllers/Issue/IssueDetailViewController.swift
+++ ios/IssueTracker/IssueTracker/Controllers/Issue/IssueDetailViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class IssueDetailViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    private lazy var dataSource = makeDataSource()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -17,4 +21,31 @@ class IssueDetailViewController: UIViewController {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(false, animated: true)
     }
+}
+
+extension IssueDetailViewController {
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, IssueViewModel>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, IssueViewModel>
+    
+    private func makeDataSource() -> DataSource {
+        guard let collectionView = collectionView else { return DataSource() }
+        return DataSource(collectionView: collectionView, cellProvider: { (collectionView, indexPath, commentViewModel) -> CommentCollectionViewCell? in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "commentCell", for: indexPath)
+            guard let listCell = cell as? CommentCollectionViewCell else { return CommentCollectionViewCell() }
+//            self.binding(cell: listCell, issueViewModel: viewModel)
+            return listCell
+        })
+    }
+    
+    func applySnapshot(animatingDifferences: Bool = true) {
+        var snapshot = Snapshot()
+        snapshot.appendSections([0])
+//        snapshot.appendItems(issueListViewModel.items, toSection: 0)
+        dataSource.apply(snapshot, animatingDifferences: animatingDifferences)
+    }
+    
+//    func binding(cell: CommentCollectionViewCell, commentViewModel: CommentViewModel) {
+//
+//    }
+    
 }
---
+++ ios/IssueTracker/IssueTracker/Views/Comment/CommentCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CommentCollectionViewCell.swift
+//  IssueTracker
+//
+//  Created by DH Park on 2020/11/08.
+//
+
+import UIKit
+
+class CommentCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var commentDateLabel: UILabel!
+    @IBOutlet weak var commentTextView: UITextView!
+    
+}
