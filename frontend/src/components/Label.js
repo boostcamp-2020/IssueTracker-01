230a8eb677d151977b52ce0a0f1a43e5e59f96e4
2hoyeong <leehodud302@naver.com>
2020-11-10 01:35:03 +0900

    [WEB] feat : 레이블 컴포넌트 구현
    레이블 컴포넌트를 구현했습니다.

---
+++ frontend/src/components/Label.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const LabelDiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 16px;
+  border: 1px solid #eaecef;
+  border-top: 0;
+`;
+
+const PreviewLabel = styled.span`
+  background: ${(props) => props.color};
+  font-size: 12px;
+  font-weight: 500;
+  padding: 0 10px;
+  line-height: 22px;
+  border: 1px solid transparent;
+  border-radius: 2em;
+`;
+
+const Description = styled.span`
+  font-size: 12px;
+  color: #586069;
+`;
+
+const ButtonDiv = styled.div`
+  display: flex;
+  width: 16.66667%;
+  justify-content: flex-end;
+`;
+
+const Button = styled.div`
+  font-size: 12px;
+  margin-left: 16px;
+  background-color: transparent;
+  border: 0;
+  white-space: nowrap;
+  color: #586069;
+`;
+
+const WidthDiv = styled.div`
+  width: ${(props) => props.width};
+`;
+
+const Label = ({ item }) => {
+  return (
+    <LabelDiv>
+      <WidthDiv width={'25%'}>
+        <PreviewLabel color={item.color}>{item.labelName}</PreviewLabel>
+      </WidthDiv>
+      <WidthDiv width={'58.33333%'}>
+        <Description>{item.description}</Description>
+      </WidthDiv>
+      <ButtonDiv>
+        <Button>Edit</Button>
+        <Button>Delete</Button>
+      </ButtonDiv>
+    </LabelDiv>
+  );
+};
+
+export default Label;
