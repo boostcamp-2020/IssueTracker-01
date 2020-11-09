7ebc26830030b5821e7242b86ab2e949197667ff
SeokHoKim <seokho0330@gmail.com>
2020-11-09 15:11:45 +0900

    [IOS] feat: IssueBottomSheetViewController 추가
    - 세부사항 bottomSheetVC 추가

---
+++ ios/IssueTracker/IssueTracker/Controllers/Issue/IssueBottomSheetViewController.swift
@@ -0,0 +1,15 @@
+//
+//  IssueBottomSheetViewController.swift
+//  IssueTracker
+//
+//  Created by 김석호 on 2020/11/09.
+//
+
+import UIKit
+
+class IssueBottomSheetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
