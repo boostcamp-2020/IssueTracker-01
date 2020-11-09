44e8279c7bc37a9595a4cc3f44efb6e91114d140
2hoyeong <leehodud302@naver.com>
2020-11-10 01:39:35 +0900

    [WEB] feat : 레이블 페이지 임시 데이터 추가
    레이블 페이지에서 임시적으로 표현될 데이터를 추가했습니다. 서버에서 전송되는 데이터와 동일한 구조로 되어있습니다.

---
+++ frontend/src/contexts/label.js
@@ -0,0 +1,32 @@
+const test = {
+  data: [
+    {
+      labelName: 'bug',
+      color: '#ff0000',
+      description: null,
+    },
+    {
+      labelName: 'hotfix',
+      color: '#00ff00',
+      description: 'hotfix',
+    },
+    {
+      labelName: 'test',
+      color: '#0000ff',
+      description: null,
+    },
+    {
+      labelName: '테스',
+      color: '#ff00ff',
+      description: null,
+    },
+    {
+      labelName: '테스트1211',
+      color: '#ff00ff',
+      description: null,
+    },
+  ],
+  message: 'success',
+};
+
+export default test;
