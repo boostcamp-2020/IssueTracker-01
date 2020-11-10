9947b5ecea8f7814bcd7750450829c67ce16789e
2hoyeong <leehodud302@naver.com>
2020-11-10 15:15:20 +0900

    [WEB] feat : 레이블 생성 컴포넌트 추가
    레이블 생성하는 컴포넌트를 추가했습니다.

--- frontend/src/templates/LabelButtons.js
+++ frontend/src/templates/LabelButtons.js
@@ -2,18 +2,23 @@ import React from 'react';
 import styled from 'styled-components';
 import TabButton from '@components/TabButton';
 import GreenButton from '@components/GreenButton';
+import LabelCreate from '@components/LabelCreate';
 
 const FlexDiv = styled.div`
   display: flex;
   justify-content: space-between;
+  margin-bottom: 20px;
 `;
 
 const LabelButtons = () => {
   return (
-    <FlexDiv>
-      <TabButton />
-      <GreenButton text={'New label'} />
-    </FlexDiv>
+    <div>
+      <FlexDiv>
+        <TabButton />
+        <GreenButton text={'New label'} />
+      </FlexDiv>
+      <LabelCreate />
+    </div>
   );
 };
 
