1a78329ca705382f2de2fddaa62c225f38fde01c
nrurnru <nrurnru@gmail.com>
2020-11-11 04:07:32 +0900

    [iOS] feat : Label 생성자 구현
    서버와 통신할 수 있도록 Label 생성자 구현

--- ios/IssueTracker/IssueTracker/Models/Label.swift
+++ ios/IssueTracker/IssueTracker/Models/Label.swift
@@ -8,5 +8,13 @@
 import Foundation
 
 struct Label: Codable, Hashable {
-    let labelName, color: String
+    let labelName: String
+    let color: String
+    let description: String?
+    
+    init(labelName: String, color: String, description: String? = nil) {
+        self.labelName = labelName
+        self.color = color
+        self.description = description
+    }
 }
