d5857b2407c3df2561d7a0cbebfd5f98a66ffbd1
jeongsangwoo <jsw0413@gmail.com>
2020-11-10 22:50:07 +0900

    [WEB] feat: 로그인 페이지 구현
    history 이용해 로그인 상태에서 로그인 페이지 접속시 main페이지로 이동기능 추가했습니다.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+import { UserProvider } from './contexts/user';
 import Login from './pages/Login';
+import Main from './pages/Main';
 
 const App = () => {
   return (
-    <div>
-      <Login />
-    </div>
+    <UserProvider>
+      <Route path="/" component={Login} exact />
+      <Route path="/main" component={Main} />
+    </UserProvider>
   );
 };
 
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import styled from 'styled-components';
 import LoginForm from '../components/login/LoginForm';
+import UserContext from '../contexts/user';
+import axios from 'axios';
 
 const Box = styled.div`
   width: 100vw;
@@ -15,7 +17,20 @@ const H1 = styled.h1`
   text-align: center;
   box-sizing: border-box;
 `;
-const Login = () => {
+const Login = ({ history }) => {
+  const { setUserId } = useContext(UserContext);
+  useEffect(() => {
+    const checkLogin = async () => {
+      try {
+        const response = await axios.get('http://127.0.0.1:3000/isLogin', { withCredentials: true });
+        setUserId(response.data.userId);
+        history.push('/main');
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    checkLogin();
+  }, []);
   return (
     <Box>
       <H1>이슈 트래커</H1>
