12a4e2b5b22b8fc06706c94012f7c34d4fbf6a5c
nrurnru <nrurnru@gmail.com>
2020-11-11 01:57:15 +0900

    [iOS] feat : 레이블 뷰 컨트롤러 화면 구현
    네트워크를 사용하지 않는 더미 레이블 표시 확인
    
    토큰 받아오기, 레이블 설명 받아오기 구현 필요

--- ios/IssueTracker/IssueTracker/Controllers/Label/LabelViewController.swift
+++ ios/IssueTracker/IssueTracker/Controllers/Label/LabelViewController.swift
@@ -8,10 +8,78 @@
 import UIKit
 
 class LabelViewController: UIViewController {
-
+    private lazy var dataSource = makeDatasource()
+    var viewModel: LabelViewModel?
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let networkManager = IssueTrackerNetworkManager.shared.self
+        let dummyVM = LabelViewModel(token: IssueTrackerGithubLoginManager.shared.token ?? "", networkManager: networkManager)
+        dummyVM.labelCellViewModels = [
+            LabelCellViewModel(label: Label(labelName: "fix", color: "#DDDDDD")),
+            LabelCellViewModel(label: Label(labelName: "bug", color: "#AADDDD")),
+            LabelCellViewModel(label: Label(labelName: "feat", color: "#DAFD12")),
+            LabelCellViewModel(label: Label(labelName: "dsa", color: "#DDDDDD")),
+            LabelCellViewModel(label: Label(labelName: "qwewqe", color: "#121212")),
+            LabelCellViewModel(label: Label(labelName: "fgxvbc", color: "#999999")),
+            LabelCellViewModel(label: Label(labelName: "aaaaaa", color: "#AABBCC")),
+            LabelCellViewModel(label: Label(labelName: "dssfdxzc", color: "#DD5566")),
+            LabelCellViewModel(label: Label(labelName: "dsdasfasd", color: "#D67822")),
+            ]
         
+        self.viewModel = dummyVM
+        configureFlowLayout()
+        applySnapshot()
     }
 
+    @IBAction func addLabel(_ sender: Any) {
+        
+    }
+}
+
+// MARK: - View identifier
+extension LabelViewController {
+    private struct ViewID {
+        static let cell = String(describing: LabelCell.self)
+    }
+}
+
+extension LabelViewController {
+    private func configureFlowLayout() {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionHeadersPinToVisibleBounds = true
+        flowLayout.itemSize = CGSize(width: self.view.frame.size.width, height: 80)
+        flowLayout.minimumLineSpacing = 1
+        
+        self.collectionView.collectionViewLayout = flowLayout
+        
+    }
+}
+
+extension LabelViewController {
+    typealias DataSource = UICollectionViewDiffableDataSource<Int, LabelCellViewModel>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, LabelCellViewModel>
+    
+    private func makeDatasource() -> DataSource {
+        guard let collectionView = collectionView else { return DataSource() }
+        return DataSource(collectionView: collectionView) { (collectionView, indexPath, viewModel) -> LabelCell? in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ViewID.cell, for: indexPath)
+            guard let listCell = cell as? LabelCell else { return LabelCell() }
+            listCell.configureCell(viewModel: viewModel)
+            
+            return listCell
+        }
+    }
+    
+    func applySnapshot(animatingDiffernces: Bool = true) {
+        guard let viewModel = viewModel else { return }
+        var snapshot = Snapshot()
+        snapshot.appendSections([0])
+        snapshot.appendItems(viewModel.labelCellViewModels)
+        dataSource.apply(snapshot, animatingDifferences: animatingDiffernces)
+        
+    }
+    
 }
--- ios/IssueTracker/IssueTracker/Views/Label/LabelCell.swift
+++ ios/IssueTracker/IssueTracker/Views/Label/LabelCell.swift
@@ -11,5 +11,14 @@ class LabelCell: UICollectionViewListCell {
     @IBOutlet weak var wrapperView: UIView!
     @IBOutlet weak var labelDescription: UILabel!
     
+    func configureCell(viewModel: LabelCellViewModel) {
+        let badge = LabelBadge(text: viewModel.labelName, colorCode: viewModel.color)
+        self.wrapperView.addSubview(badge)
+        //FIXME: 라벨 설명을 받아오는 방법
+        self.labelDescription.text = "라벨 설명"
+    }
     
+    override func prepareForReuse() {
+        wrapperView.subviews.forEach{ $0.removeFromSuperview() }
+    }
 }
