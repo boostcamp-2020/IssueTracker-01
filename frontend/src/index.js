8157cd3037f43d9e5a0846c61e5c00b967fe56ff
ohmink <kkop5056@naver.com>
2020-11-11 22:34:37 +0900

    [Web] feat: 마일스톤 관련 화면 추가 BrowserRouter안에 마일스톤 context와 마일스톤 화면들을 추가하였다.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,18 @@
 import React from "react";
 import ReactDom from "react-dom";
-import App from "./App.js";
+import { BrowserRouter, Route } from 'react-router-dom';
+import Milestone from './pages/Milestone';
+import MilestoneAdding from './pages/MilestoneAdding';
+import MilestoneEditing from './pages/MilestoneEditing';
+import MilestoneProvider from './contexts/milestone';
 
-ReactDom.render(<App />, document.getElementById("root"));
+ReactDom.render(
+    <BrowserRouter>
+      <MilestoneProvider>
+        <Route path="/milestone" exact={true} component={Milestone}/>
+        <Route path="/milestone-add" component={MilestoneAdding}/>
+        <Route path="/milestone-edit" component={MilestoneEditing}/>
+      </MilestoneProvider>
+    </BrowserRouter>,
+    document.getElementById('root')
+  );
