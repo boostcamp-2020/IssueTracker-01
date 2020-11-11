43fd734d21a6dcfd2520618e00cbf4b45b6b2c5b
ohmink <kkop5056@naver.com>
2020-11-11 22:20:52 +0900

    [Web] style: 주석 삭제 익스프레스 제네레이터로 만들어진 주석과 불필요한 주석을 삭제하였다.

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 import createError from 'http-errors';
 import express from 'express';
-// import path from 'path';
+
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 
@@ -20,24 +20,21 @@ sequelize
   .catch((err) => {
     console.error(err);
   });
+
 passportConfig();
-// view engine setup
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
 app.use('/', indexRouter);
 
-// catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404));
 });
 
-// error handler
 app.use((err, req, res) => {
-  // render the error page
   res.status(err.status || 500).json({ message: err.message });
 });
 
