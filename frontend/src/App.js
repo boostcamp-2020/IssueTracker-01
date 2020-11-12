f8f2e42ccbebfc9b1797f990f88df7fc5463c1bc
ohmink <kkop5056@naver.com>
2020-11-12 21:46:15 +0900

    [Web] feat: 라우터 설정 각 컨텍스트와 라우터 경로에 대한 코드를 추가하였다.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import { UserProvider } from './contexts/user';
+
 import Login from './pages/Login';
 import Main from './pages/Main';
 import Label from '@pages/Label';
+import IssueAdding from './pages/IssueAdding';
+import Milestone from './pages/Milestone';
+import MilestoneAdding from './pages/MilestoneAdding';
+import MilestoneEditing from './pages/MilestoneEditing';
+
+import { UserProvider } from './contexts/user';
 import { LabelProvider } from '@contexts/label';
+import UserListProvider from './contexts/userList';
+import SelectionProvider from './contexts/selection';
+import MilestoneProvider from './contexts/milestone';
 
 const App = () => {
   return (
     <UserProvider>
       <LabelProvider>
         <Route path="/" component={Login} exact />
-        <Route path="/main" component={Main} />
-        <Route path="/label" component={Label}
+        <Route path="/label" component={Label} />
+        <MilestoneProvider>
+          <Route path="/milestone" exact={true} component={Milestone}/>
+          <Route path="/milestone-add" component={MilestoneAdding}/>
+          <Route path="/milestone-edit" component={MilestoneEditing}/>
+          <UserListProvider>
+            <SelectionProvider>
+              <Route path="/issue-add" component={IssueAdding}/>
+            </SelectionProvider>
+            <Route path="/main" component={Main} />
+          </UserListProvider>
+        </MilestoneProvider>
       </LabelProvider>
     </UserProvider>
   );
