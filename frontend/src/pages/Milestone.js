4fee62f919e91a23263bd25b72007b859493fe47
ohmink <kkop5056@naver.com>
2020-11-13 11:53:05 +0900

    버튼 추가

--- frontend/src/pages/Milestone.js
+++ frontend/src/pages/Milestone.js
@@ -2,16 +2,30 @@ import React from 'react';
 import styled from 'styled-components';
 import TableListHeader from '../organisms/milestone/TableListHeader';
 import TableList  from '../organisms/milestone/TableList';
+import TabButton from '../components/label/TabButton';
+import GreenButton from '@components/utils/GreenButton';
+
 
 const MilestonePage = styled.div`
     width: 78%;
     margin: 0 auto;
 `;
 
+const Buttons = styled.div`
+  display: flex;
+  margin-left: 8px;
+  padding-left: 8px;
+  justify-content: flex-end;
+`;
+
 const Milestone = () => {
     return <MilestonePage>
-            <TableListHeader/>
-            <TableList/>
+        <Buttons>
+            <TabButton/>
+            <GreenButton text="New milestone" onClick={() => (location.href = '/milestone-add')} />
+        </Buttons>
+        <TableListHeader/>
+        <TableList/>
     </MilestonePage>
 }
 
