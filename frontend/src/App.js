f0024e6a5d3c19eec9f740be6574d13561441f11
2hoyeong <leehodud302@naver.com>
2020-11-13 11:13:45 +0900

    [WEB] fix : issue context 추가 안한거 수정
    issue context를 추가안한것을 수정했습니다.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import DetailIssue from './pages/DetailIssue';
 
 import { UserProvider } from './contexts/user';
 import { LabelProvider } from '@contexts/label';
+import { IssueProvider } from '@contexts/issue';
 import UserListProvider from './contexts/userList';
 import SelectionProvider from './contexts/selection';
 import MilestoneProvider from './contexts/milestone';
@@ -19,22 +20,24 @@ import MilestoneProvider from './contexts/milestone';
 const App = () => {
   return (
     <UserProvider>
-      <LabelProvider>
-        <Route path="/" component={Login} exact />
-        <Route path="/label" component={Label} />
-        <MilestoneProvider>
-          <Route path="/milestone" exact={true} component={Milestone} />
-          <Route path="/milestone-add" component={MilestoneAdding} />
-          <Route path="/milestone-edit" component={MilestoneEditing} />
-          <UserListProvider>
-            <SelectionProvider>
-              <Route path="/issue-add" component={IssueAdding} />
-            </SelectionProvider>
-            <Route path="/main" component={IssueMain} />
-          </UserListProvider>
-        </MilestoneProvider>
-      </LabelProvider>
-      <Route path="/detailIssue/:issueId" component={DetailIssue} />
+      <IssueProvider>
+        <LabelProvider>
+          <Route path="/" component={Login} exact />
+          <Route path="/label" component={Label} />
+          <MilestoneProvider>
+            <Route path="/milestone" exact={true} component={Milestone} />
+            <Route path="/milestone-add" component={MilestoneAdding} />
+            <Route path="/milestone-edit" component={MilestoneEditing} />
+            <UserListProvider>
+              <SelectionProvider>
+                <Route path="/issue-add" component={IssueAdding} />
+              </SelectionProvider>
+              <Route path="/main" component={IssueMain} />
+            </UserListProvider>
+          </MilestoneProvider>
+        </LabelProvider>
+        <Route path="/detailIssue/:issueId" component={DetailIssue} />
+      </IssueProvider>
     </UserProvider>
   );
 };
