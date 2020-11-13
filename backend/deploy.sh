1f95f9815e16bd30e52de8449a4dde5267048b03
2hoyeong <leehodud302@naver.com>
2020-11-13 10:41:07 +0900

    [WEB] feat : 배포용 스크립트 추가
    배포용 스크립트입니다.

---
+++ backend/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for PID in $(pgrep -fl node | awk '{print $1}'); do
+    echo "> kill -15 $PID"
+    kill -15 $PID
+    sleep 5
+done
+
+npm --prefix /root/src/frontend install
+npm --prefix /root/src/backend install
+nohup npm --prefix /root/src/frontend start 2>&1 &
+nohup npm --prefix /root/src/backend start 2>&1 &
